e9d86875b421e1f2e81fafcb26400bb5a6d4e76e
bijoy1295 <senbijoy173@gmail.com>
2025-08-02 13:36:29 +0600

    Circle.cpp

---
+++ circle.cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int r;
+    cout<<"Enter the value "<<endl;
+    cin>>r;
+    float result;
+    result=3.1416*r*r;
+    cout<<"The Circle Area: "<<result;
+    return 0;
+}
